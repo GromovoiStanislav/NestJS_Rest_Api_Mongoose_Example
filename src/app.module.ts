6ebf946e237d9ae9c8207442daf11fdf6df11b93
Gromovoi Stanislav <gromila12@rambler.ru>
2023-04-19 09:48:42 +0600

    refactoring

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ItemsController } from './items/items.controller';
 import { ItemsService } from './items/items.service';
 import { ItemsModule } from './items/items.module';
@@ -9,7 +7,5 @@ import config from './config/keys';
 
 @Module({
   imports: [ItemsModule, MongooseModule.forRoot(config.mongoURI)],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
